1e2f9089650b036e148715c758335ea203138627
Rohit Garg <rohitgarg19@gmail.com>
2015-12-21 18:36:48 +0530

    Added autopilot in cache

--- app/models/file_downloader.rb
+++ app/models/file_downloader.rb
@@ -2,7 +2,11 @@ class FileDownloader
   def self.enqueue_download song_url, song_request
     if File.exist? "songs/#{song_request.file_id}.mp3"
       song_request.status = 'Downloaded'
-      song_request.save!
+      if ENV['autopilot']
+        song_request.enqueue!
+      else
+        song_request.save!
+      end
     else
       Thread.new do
         %x{mkdir -p songs && youtube-dl -o songs/#{song_request.file_id}.%\\(ext\\)s -x --audio-format mp3 #{song_url}}
