0ca45a68a3ff91a192b3cf0a44d2265b40439001
Rohit Garg <rohitgarg19@gmail.com>
2015-12-22 16:06:52 +0530

    Just being explicit

--- app/models/file_downloader.rb
+++ app/models/file_downloader.rb
@@ -2,7 +2,7 @@ class FileDownloader
   def self.enqueue_download song_url, song_request
     if File.exist? "songs/#{song_request.file_id}.mp3"
       song_request.status = 'Downloaded'
-      if ENV['autopilot']
+      if ENV['autopilot'] == 'true'
         song_request.enqueue!
       else
         song_request.save!
@@ -12,7 +12,7 @@ class FileDownloader
         %x{mkdir -p songs && youtube-dl -o songs/#{song_request.file_id}.%\\(ext\\)s -x --audio-format mp3 #{song_url}}
         if File.exist? "songs/#{song_request.file_id}.mp3"
           song_request.status = 'Downloaded'
-          if ENV['autopilot']
+          if ENV['autopilot'] == 'true'
             song_request.enqueue!
           end
         else
